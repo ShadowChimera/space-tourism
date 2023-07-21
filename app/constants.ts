3e76cc859fe3d0bc70326917821419e12605516c
Alexey Babenko <alexey22042001@gmail.com>
2023-07-21 17:28:30 +0300

    Add enum to distinguish pages

--- app/constants.ts
+++ app/constants.ts
@@ -1,18 +1,30 @@
+export enum Pages {
+  home = 'home',
+  destination = 'destination',
+  crew = 'crew',
+  technology = 'technology',
+}
+
 export const mainNav = [
   {
+    name: Pages.home,
     text: 'home',
     href: '/',
   },
   {
+    name: Pages.destination,
     text: 'destination',
     href: '/destination',
   },
   {
+    name: Pages.crew,
     text: 'crew',
     href: '/crew',
   },
   {
+    name: Pages.technology,
     text: 'technology',
     href: '/technology',
   },
 ];
+
