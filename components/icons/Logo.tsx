66a748390f5724d36f0a19a09606d364c8ce7cce
Alexey Babenko <alexey22042001@gmail.com>
2023-07-21 17:25:30 +0300

    Add priority attribute

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -2,7 +2,7 @@ import Image, { ImageProps } from 'next/image';
 import logo from '@/public/static/shared/logo.svg';
 
 export const Logo = (props: Partial<ImageProps>) => {
-  return <Image {...props} src={logo} alt="Logo" />;
+  return <Image {...props} src={logo} alt="Logo" priority />;
 };
 
 export default Logo;
